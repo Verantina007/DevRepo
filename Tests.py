4b5b670a94b83b30fda2e62ba44f53d8e7d1c65f
Verantina007 <105320531+Verantina007@users.noreply.github.com>
2022-06-23 16:41:12 +0300

    Update Tests.py

--- Tests.py
+++ Tests.py
@@ -13,7 +13,7 @@ unittest.main(
 
 class TestStringMethods(unittest.TestCase):
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'fOO')
+        self.assertEqual('foo'.upper(), 'FOO')
 
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
